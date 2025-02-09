f12987918ab0515c05ccbcf705e1aec6bb537edd
Alexandr Primak <primak.aa@gmail.com>
2025-02-10 01:26:45 +0300

    small improvements.

--- namesilo_ddns.sh
+++ namesilo_ddns.sh
@@ -26,10 +26,15 @@ fi
 
 if [ "$cur_ip" != "$known_ip" ]; then
   echo $cur_ip > $ip_file
-  logger -t ddns_check -- subdomain ip changed to $cur_ip for $subdomain.$domain, let\'s try updating
+  logger -t ddns_check -- subdomain ip changed to $cur_ip for $full_name, let\'s try updating
 
   ## Update DNS record in Namesilo:
   record_id=$(curl -s "https://www.namesilo.com/api/dnsListRecords?version=1&type=json&key=${apikey}&domain=${domain}" | jq -r --arg host ${full_name} '.reply.resource_record[] | select(.host==$host).record_id')
+  if [ -z "${record_id}" ]; then
+    logger -t ddns_check -- update failed: record_id is empty \(add domain to the registar firts\)!
+    exit 1
+  fi
+
   response_code=$(curl -s "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=json&key=${apikey}&domain=${domain}&rrid=${record_id}&rrsubdomain=${subdomain}&rrvalue=${cur_ip}&rrttl=3600" | jq -r '.reply.code')
 
   case $response_code in
